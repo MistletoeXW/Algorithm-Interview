afa52ece81f47e16e4a612d4d2a25975127f346b
MistletoeXW <15178931042@qq.com>
2020-02-01 18:30:08 +0800

    Day16

--- src/OfferCode/T3FindRangeNum.java
+++ src/OfferCode/T3FindRangeNum.java
@@ -1,8 +1,10 @@
 package OfferCode;
 
+import org.omg.CORBA.PUBLIC_MEMBER;
+
 import java.util.HashSet;
-import java.util.Queue;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 /**
  * @ClassName: T3FindRangeNum
@@ -10,8 +12,19 @@ import java.util.Scanner;
  *                      请找到数组中重复的数字
      *               方法一: Hash法,时间复杂度为O(n),空间复杂度为O(n)
      *               方法二: 排序法,先进行排序,再进行遍历, 使用快排或者归并或者堆排序时间复杂度为O(nlogn),空间复杂度看情况
-     *               方法三: 数据映射法, 将数组中的值作为数组下标进行遍历,同时将对应下标的值改为相反数,
+     *               方法三: 数据映射法 时间复杂度为O(n) 空间复杂度为O(1)
+ *                          将数组中的值作为数组下标进行遍历,同时将对应下标的值改为相反数,
      *                      如果遍历到相应位置的值为负数,则表示此数字重复
+ *               上述方法中,都需要改变数组, 如果不想改变数组值也只能另外开辟一个数组空间
+ *
+ *               以下给出不改变数组的方法: 二分查找法
+ *                          以长度为8的数组{2,3,5,4,3,2,6,7}为例分析查找过程
+ *                          1. 首先根据题目要求,这长度为8的数组要求里面的元素都在1~7范围内
+ *                          2. 求出中间数字4, 把1~7分为分为两段,一段是1~4,另一段是5~7
+ *                          3. 接下来统计1~4这4个数字在数组中出现的次数,它们共出现了5次,因此1~4必有重复
+ *                          4. 接下来再把1~4一分为二,一段是1~2,一段是3~4
+ *                          5. 接下来统计两者,发现3和4总共出现了3次,所以重复法数字肯定在3和4中,我们再统计3和4的出现的次数
+ *                             可以得出重复的数字
  * @Author:xuwen
  * @Date: 2020/2/1 下午2:40
  **/
@@ -120,6 +133,54 @@ public class T3FindRangeNum {
         return i;
     }
 
+    //======================方案四:二分查找法===========================
+    /*
+     * @Author: xw
+     * @Description: 统计范围中的元素出现的次数//TODO
+     * @Date: 下午5:33 2020/2/1
+     * @Param: [arr, start, end]
+     * @Return: int
+     **/
+    public static int countRange(int[] arr,int start,int end){
+        int count = 0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] >= start && arr[i] <= end)
+                count++;
+
+        }
+        return count;
+    }
+    /*
+     * @Author: xw
+     * @Description: 二分查找法进行判断找出重复元素//TODO
+     * @Date: 下午5:39 2020/2/1
+     * @Param: [arr]
+     * @Return: int
+     **/
+    public static int findRangeNum_4(int[] arr){
+
+        int start =1;
+        int end = arr.length-1;
+        while (start <= end){
+
+            int middle = (start+end) / 2;
+            int count = countRange(arr,start,middle);
+            if(start == end){
+                if(count > 1)
+                    return start;
+                else
+                    break;
+            }
+
+            if(count > (middle-start+1))
+                end = middle;
+            else
+                start = middle+1;
+
+        }
+        return -1;
+
+    }
 
 
     //==============================主函数===============================
@@ -134,17 +195,12 @@ public class T3FindRangeNum {
             arr[i] = Integer.parseInt(a[i]);
         }
 
-        int num = findRangeNum_3(arr);
+        int num = findRangeNum_4(arr);
         if (num != -1) {
             System.out.print("重复的数字为: " + num+"\n");
         }else
             System.out.print("数组中没有重复的数字\n");
 
-
-
     }
-
-
-
 
 }
